2fed37020f3f5b78faf862177d60acf24c5c8ec6
alfael <alfael.sc@gmail.com>
2021-09-29 14:48:57 +0200

    Add Build script

---
+++ build.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+cd $(dirname $0)
+export PATH="$HOME/toolchain/eva/bin:$PATH"
+export PATH="$HOME/toolchain/eva64/bin:$PATH"
+export CROSS_COMPILE=aarch64-elf-
+export CROSS_COMPILE_ARM32=arm-eabi-
+export ARCH=arm64
+export SUBARCH=arm64
+export KBUILD_CFLAGS="-Wno-maybe-uninitialized -Wno-memset-elt-size -Wno-duplicate-decl-specifier"
+if [ ! -d "./output/" ]; then
+        mkdir ./output/
+fi
+rm -r ./output/*
+make O=output clean
+make O=output mrproper
+make O=output basik_defconfig
+make O=output -j$(nproc --all) 2>&1 | tee build.log
+
+PATH_OUTPUT=output/arch/arm64/boot
+PATH_KERN=$PATH_OUTPUT/Image.gz
+PATH_QCOM=$PATH_OUTPUT/dts/qcom/
+PATH_PACKAGE=package
+PATH_OUTPUT_PACKAGE=~/kernel/output
+
+if [ ! -f "$PATH_KERN" ]; then
+        echo Erreur de compilation avortement...
+        exit;
+fi
+echo Compilation terminée !
+echo Création du package flashable...
+while read line
+do
+        if grep -q "SUBLEVEL = " <<< "$line"; then
+                SUBLEVEL=$(cut -d "=" -f2 <<< "$line" | xargs)
+                continue;
+        fi
+        if [ ! -z "$SUBLEVEL" ]; then
+                break;
+        fi
+done < Makefile
+while read line
+do
+        if grep -q "LOCALVERSION=" <<< "$line"; then
+                LOCALVERSION=$(cut -d "-" -f2 <<< "$line" | xargs -0 | tr -d '"')
+                continue;
+        fi
+        if [ ! -z "$LOCALVERSION" ]; then
+                break;
+        fi
+done < arch/arm64/configs/basik_defconfig
+
+/bin/cp -rf $PATH_KERN $PATH_PACKAGE/kernel
+/bin/cp -rf $PATH_QCOM/msm8953-qrd-sku3-tissot-treble.dtb $PATH_PACKAGE/dtb-treble
+/bin/cp -rf $PATH_QCOM/msm8953-qrd-sku3-tissot-nontreble.dtb $PATH_PACKAGE/dtb-nontreble
+cd $PATH_PACKAGE
+zip -9 -r $PATH_OUTPUT_PACKAGE/$LOCALVERSION.$SUBLEVEL.zip ./*
+cd -;
+echo Création du package: $LOCALVERSION.$SUBLEVEL.zip terminée !
